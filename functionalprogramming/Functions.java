ed94add9ab23db95b6292165b332591de4ea2021
Tilak-Deety <tilakdeety@gmail.com>
2022-12-03 20:31:46 +0530

    working with functional interfaces

---
+++ functionalprogramming/Functions.java
@@ -0,0 +1,10 @@
+package functionalprogramming;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class Functions {
+    public static void main(String[] args) {
+        System.out.println(greetFunction.apply("Welcome "));
+    }
+    static Function<String, String> greetFunction = message -> message + "Tilak";
+}
